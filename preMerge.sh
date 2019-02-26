388f4f87b11e9d24a89616125de707cd74eac7a9
Christopher Lloyd <christopher.lloyd@caci.com>
2019-02-26 17:50:01 +0000

    Updated changelog

--- preMerge.sh
+++ preMerge.sh
@@ -1,15 +1,22 @@
 
-while getopts b: option
+while getopts b:c: option
 do
 case "${option}"
 in
 b) SOURCE_BRANCH=${OPTARG};;
+c) COMMIT=${OPTARG};;
 esac
 done
 
 if [ -z "$SOURCE_BRANCH" ]
 then
-echo "You must specify a source branch (i.e spin-09) using './preMerge.sh -b <source_branch_name>'"
+echo "You must specify a source branch (i.e spin-09) using './preMerge.sh -b <source_branch_name> -c <commit_message>'"
+exit 1
+fi
+
+if [ -z "$COMMIT" ]
+then
+echo "You must specify a commit message. './preMerge.sh -b <source_branch_name> -c <commit_message>'"
 exit 1
 fi
 
@@ -20,6 +27,8 @@ PACKAGE_VERSION=$(cat package.json \
   | head -1 \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g')
+git add --all
+git commit -m $COMMIT
 
 # - Squash commits
 git rebase -i $SOURCE_BRANCH
@@ -27,6 +36,8 @@ git rebase -i $SOURCE_BRANCH
 # - Generate changelog
 rm -f CHANGELOG.md
 npm run changelog
+git add --all
+git commit -m "Updated changelog"
 
 # - Squash again
 git rebase -i $SOURCE_BRANCH
